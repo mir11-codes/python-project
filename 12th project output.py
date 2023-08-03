f986f6ad2d7b6b5f7ce419d6767c8d02d70041da
mir11-codes <137433929+mir11-codes@users.noreply.github.com>
2023-08-03 19:40:02 +0530

    Add files via upload

---
+++ 12th project output.py
@@ -0,0 +1,399 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 22:39:24) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\user\AppData\Local\Programs\Python\Python38-32\12THLIB PROG.py
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 2
+      ISBN        Book         Price      Author    Publisher    Status
+================================================================================
+         12      potter         121     rowling bloomsburry   available 
+         13        nemo          56     charlie     penguin   available 
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 5
+enter isbn value: 12
+do you want to delete?(y/n): y
+record deleted
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 2
+      ISBN        Book         Price      Author    Publisher    Status
+================================================================================
+         13        nemo          56     charlie     penguin   available 
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 5
+enter isbn value: 13
+do you want to delete?(y/n): y
+record deleted
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 3
+enter isbn no.: 12
+enter book name: potter
+enter price: 121
+enter author's name: rowling
+enter publisher: bloomsburry
+record added
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 2
+      ISBN        Book         Price      Author    Publisher    Status
+================================================================================
+         12      potter         121     rowling bloomsburry   available 
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 3
+enter isbn no.: 13
+enter book name: nemo
+enter price: 56
+enter author's name: charlie
+enter publisher: penguin
+record added
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 1
+enter isbn value: 13
+       ISBN No.    Book Name    Price     Author    Publisher        Status
+================================================================================
+        13            nemo         56     charlie      penguin        available
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 4
+enter isbn value: 12
+book:  potter
+modify book name?(y/n): n
+price:  121
+modify price?(y/n): y
+enter new price: 400
+record modified
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 2
+      ISBN        Book         Price      Author    Publisher    Status
+================================================================================
+         12      potter         400     rowling bloomsburry   available 
+         13        nemo          56     charlie     penguin   available 
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 3
+enter isbn no.: 14
+enter book name: percy jackson
+enter price: 99
+enter author's name: riordan
+enter publisher: penguin
+record added
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 2
+      ISBN        Book         Price      Author    Publisher    Status
+================================================================================
+         12      potter         400     rowling bloomsburry   available 
+         13        nemo          56     charlie     penguin   available 
+         14 percy jackson          99     riordan     penguin   available 
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 5
+enter isbn value: 14
+do you want to delete?(y/n): y
+record deleted
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 6
+enter isbn value: 12
+book name:  potter
+price:  400
+author name:  rowling
+publisher:  bloomsburry
+availability:  available
+do you want to proceed?(y/n): y
+availability:  issued
+book issued
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 2
+      ISBN        Book         Price      Author    Publisher    Status
+================================================================================
+         12      potter         400     rowling bloomsburry      issued 
+         13        nemo          56     charlie     penguin   available 
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 7
+enter isbn value: 13
+book already available
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 7
+enter isbn value: 12
+book name:  potter
+price:  400
+author name:  rowling
+publisher:  bloomsburry
+availability:  issued
+do you want to proceed?(y/n): Y
+is the book date expired?(y/n):Y
+by how many days is the book returned late? 4
+           RECEIPT           
+Final Total: 408
+Availability: available
+*******************************
+book returned
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 8
+thanks for using library management system
+>>> 
+= RESTART: C:\Users\user\AppData\Local\Programs\Python\Python38-32\12THLIB PROG.py
+                                                                      
+                         LIBRARY MANAGEMENT                           
+                          ARCHIE'S LIBRARY                          
+                                                                    
+                            MAINTENANCE                             
+1. view book details by ISBN
+2. view all books
+3. add book details 
+4. edit book details
+5. remove book
+                             LIBRARY                                  
+6. Issue book
+7. return book
+**********************************************************************
+8. Exit
+Enter  choice (1/2/3/4/5/6/7/8): 8
+thanks for using library management system
+>>> 
